d9a5827344c0d26b80a33a7bfd5cf5711fb1205d
Danilo Riffo <danriffo@gmail.com>
2020-09-10 15:52:14 -0300

    updated firebase keys and improved styling

--- js/firepad_init.js
+++ js/firepad_init.js
@@ -1,9 +1,10 @@
 function init() {
     //// Initialize Firebase.
     var config = {
-        apiKey: "AIzaSyC_JdByNm-E1CAJUkePsr-YJZl7W77oL3g",
-        authDomain: "firepad-tests.firebaseapp.com",
-        databaseURL: "https://firepad-tests.firebaseio.com"
+        apiKey: "AIzaSyBfcn0QLjIsWxc9V2E8aWOKWshiEwZo4Ac",
+        authDomain: "html-python-text-editor.firebaseapp.com",
+        projectId: "html-python-text-editor",
+        databaseURL: "https://html-python-text-editor.firebaseio.com"
     };
     firebase.initializeApp(config);
     //// Get Firebase Database reference.
@@ -21,30 +22,6 @@ function init() {
         defaultText: 'print("Hello, world!")'
     });
 
-    //run code when the run button is submitted
-    $("#run-code").click(function() {
-        var password = $("#pswd").val()
-        var code = firepad.getText();
-        $.ajax({
-            url: "php/run_code.php",
-            type: "POST",
-            dataType: "json",
-            data: {
-                "password": password,
-                "program-code": code
-            }
-        }).done(function(data) {
-            $("#code-output").empty();
-            for (var key in data) {
-                $("#code-output").append(data[key] + "\n");
-            }
-            $('html,body').animate({scrollTop: $("#output").offset().top}, 800);
-            $("#output").effect("shake",{direction: "down", times: 1, distance: 10}, 500);
-        }).fail(function(data) {
-            console.log("oh no!");
-            console.log(data);
-        });
-    });
 }
 // Helper to get hash from end of URL or generate a random one.
 function getExampleRef() {
